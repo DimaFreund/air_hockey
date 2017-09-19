b9eaccaa047f4b644bced7567097289367623046
DimaFreund <dimafreund@ukr.net>
2017-09-19 17:17:56 +0300

    start version

--- app/js/common.js
+++ app/js/common.js
@@ -1,33 +1,86 @@
-$(function() {
-
-	$('#top-background-carousel').owlCarousel({
-    loop: true,
-    items: 1,
-    nav: true,
-  	navText: ['<i class="fa fa-chevron-left" aria-hidden="true"></i>','<i class="fa fa-chevron-right" aria-hidden="true"></i>'],
-   	autoplay: true,
-})
-
-
-	$('#teams-carousel').owlCarousel({
-    loop: true,
-    margin: 20,
-    items: 4,
-    nav: true,
-  	navText: ['<i class="fa fa-chevron-left" aria-hidden="true"></i>','<i class="fa fa-chevron-right" aria-hidden="true"></i>'],
-   	autoplay: true,
-})
-
-
-$(".main").onepage_scroll({
-   sectionContainer: "section", // контейнер, к которому будет применяться скролл
-   easing: "ease", // Тип анимации "ease", "linear", "ease-in", "ease-out", "ease-in-out"
-   animationTime: 1000, // время анимации
-   pagination: true, // скрыть или отобразить пагинатор
-   updateURL: false // обновлять URL или нет
-
-});
+var width = window.innerWidth; //получаем ширину экрана
+var height = window.innerHeight; // получаем высоту экрана
+var app; //создаем глобальную переменную нашей игры
+var colors = [0x111111]; //массив цветов
+var gravity = 4;
+var figuresAmount = -1; //количество созданных фигур
+var figure = []; //массив хранящий нашу фигуру
 
 
 
-});
+var model = {
+    createCanvas: function() {
+        app = new PIXI.Application(width, height); //создае холст
+        document.body.appendChild(app.view); //выводим его в тело страницы
+    },
+    drawCircle: function(circleX, circleY) {
+        rand = Math.floor(Math.random() * colors.length); //генерим рандомное число(в промежутке от 0 до количества цветов в массиве цветов)
+        var radius = 30; //радиус круга
+       
+       
+        var circle = new PIXI.Graphics(); //создаем новый графический элемент
+        circle.lineStyle(0); //начинаем рисовать
+        circle.beginFill(colors[rand], 1); //задаем рандомный цвет
+        circle.drawCircle(circleX, circleY, radius); //рисуем кружок, ведь он наш дружок
+        circle.endFill(); //закончили отрисовку
+        circle.interactive = true; //делаем круг интерактивным
+        circle.buttonMode = true; //меняем курсор при наведении
+        circle.live = true; //указываем что наш шарик жив и не пал жертвой выстрела
+        //figuresAmount++;
+        //circle.num = figuresAmount; //даем нашему кругу порядковый номер
+        figure.push(circle); //обратиться на прямую к объекту circle мы не можем, поэтому отправляем его в массив
+        app.stage.addChild(circle); //выводим круг на холсте
+        circle.on('pointerdown', controller.clearFigure); //добавляем возможность при клике на фигуру удалить её
+        return circle;
+    },
+    gameOver: function() {
+        var style = new PIXI.TextStyle({ //стили для текста
+            fill: '0xffffff',
+            fontSize: 36,
+        }); 
+        var gameOverText = new PIXI.Text('Game Over', style); //собственно выводимый текст
+        gameOverText.x = width / 2; //центрируем относительно экрана
+        gameOverText.y = height / 2; //центрируем относительно экрана
+        gameOverText.pivot.x = 50; //выравниваем по оси х
+        gameOverText.pivot.y = 50; // выравниваем по оси y
+        app.stage.addChild(gameOverText); //выводим на холсте
+    }
+}
+var firstplayer = {
+    createplayer: function() {    
+      player = model.drawCircle(70, height/2);
+      player.mouseover = function(Data){
+        console.log(Data);
+      }; 
+    }
+}
+var view = {
+    loadGame: function() {
+        model.createCanvas();
+        firstplayer.createplayer();
+        
+        //setInterval(model.drawCircle, 500);
+
+        app.ticker.add(function() { //постоянное обновление холста
+            for (var i = 0; i < figuresAmount; i++) {
+                figure[i].position.y += gravity; //заставляем гравитацию работать
+                if (figure[i].position.y > height && figure[i].live == true) {
+                    model.gameOver();
+                    return false;
+                }
+
+            }
+        });
+    }
+}
+
+
+var controller = {
+    clearFigure: function() {
+        this.clear();
+        figure[this.num].live = false;
+
+    } 
+}
+
+view.loadGame();
